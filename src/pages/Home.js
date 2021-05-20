d0eea6519a54bece9b0e0330cd3a87efffb2dc22
Ankit Patel <ankitpatelak2017@gmail.com>
2021-05-20 14:21:17 +0530

    added search option

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,22 +5,28 @@ import { apiGet } from '../misc/config';
 const Home = () => {
 
     const [input, setInput] = useState("");
-
     const [results, setResults] = useState(null);
+    const [searchOption, setSearchOption] = useState("shows")
+
+    const isShowSearch = searchOption === "shows";
 
     const onInputChange = (e) => {
-        setInput(e.target.value);
+        setInput(e.target.value)
+    }
+
+    const onRadioClick = (e) => {
+        setSearchOption(e.target.value)
     }
 
     const onSearch = () => {
-        apiGet(`/search/shows?q=${input}`).then(result => {
-            setResults(result);
+        apiGet(`/search/${searchOption}?q=${input}`).then(result => {
+            setResults(result)
         });
     }
 
     const onKeyDown = (e) => {
         if (e.keyCode === 13) {
-            onSearch();
+            onSearch()
         }
     }
 
@@ -30,22 +36,24 @@ const Home = () => {
                 <div>
                     No Results
                 </div>
-            );
+            )
         }
 
         if (results && results.length > 0) {
             return (
-                <div>
-                    {
-                        results.map((item) => {
-                            return (
-                                <div key={item.show.id}>{item.show.name}</div>
-                            )
-                        }
+                results[0].show ?
+                    results.map((item) => {
+                        return (
+                            <div key={item.show.id}>{item.show.name}</div>
                         )
-                    }
-                </div>
-            )
+                    })
+                    :
+                    results.map((item) => {
+                        return (
+                            <div key={item.person.id}>{item.person.name}</div>
+                        )
+                    })
+            );
         }
     }
 
@@ -53,6 +61,17 @@ const Home = () => {
     return (
         <MainPageLayout>
             <input type="text" onChange={onInputChange} value={input} onKeyDown={onKeyDown} />
+            <div>
+                <label htmlFor="shows-search">
+                    Shows
+                    <input type="radio" id="shows-search" value="shows" checked={isShowSearch} onChange={onRadioClick} />
+                </label>
+
+                <label htmlFor="actors-search">
+                    Actors
+                    <input type="radio" id="actors-search" value="people" checked={!isShowSearch} onChange={onRadioClick} />
+                </label>
+            </div>
             <button type="button" onClick={onSearch} >Search</button>
             {renderResult()}
         </MainPageLayout>
